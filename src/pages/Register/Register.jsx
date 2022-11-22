262d18beba48932591ce579aecc5641b25c55de7
Divyansh Agarwal <dagarwal058@gmail.com>
2022-11-22 20:19:28 +0530

    jsx name change

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -8,7 +8,8 @@ function Register() {
 
      function handleSubmit(e)
      {
-        console.log(user.name);
+      console.log(user.name);
+        console.log(user.address);
      }
 
       return(
@@ -38,53 +39,77 @@ function Register() {
 
                   <div className="mb-3">
                       <label htmlFor="" className="form-label">Email</label>
-                      <input type="email" name="" className="form-control" />
+                      <input onInput={e => {
+                        user.email = e.target.value;
+                      }} type="text" name="email" className="form-control" />
                   </div>
 
+                  
                   <div className="mb-3">
                       <label htmlFor="" className="form-label">UserName</label>
-                      <input type="text" name="" className="form-control" />
+                      <input 
+                      onInput={e => {
+                        user.userName = e.target.value;
+                      }}
+                      type="text" name="userName" className="form-control" />
                   </div>
 
                   <div className="mb-3">
                       <label htmlFor="" className="form-label">Password</label>
-                      <input type="password" name="" className="form-control" />
+                      <input 
+                      onInput={e => {
+                        user.password = e.target.value;
+                      }}
+                      type="password" name="password" className="form-control" />
                   </div>
 
                   <div className="mb-3">
                       <label htmlFor="" className="form-label">Phone</label>
-                      <input type="number" name="" className="form-control" />
+                      <input onInput={e => {
+                        user.password = e.target.value;
+                      }} type="number" name="phone" className="form-control" />
                   </div>
 
-                  <div className="mb-3">
-                      <label htmlFor="" className="form-label"></label>
-                      <input type="text" name="" className="form-control" />
-                  </div>
 
                   <h4>Address</h4>
                   <br />
                   <div className="row">
                   <div className="mb-3 col-6">
-                      <input type="text" name="" className="form-control" placeholder='Flat no'/>
+                      <input onInput={e => {
+                        if(!user.address) user.address = {};
+                        user.address.flatNo = e.target.value;
+                      }} type="number" name="flatNo" className="form-control" placeholder='Flat no'/>
                   </div>
 
                   <div className="mb-3 col-6">
-                      <input type="text" name="" className="form-control" placeholder='Street/Apartment'/>
+                      <input onInput={e => {
+                        if(!user.address) user.address = {};
+                        user.address.street = e.target.value;
+                      }} type="text" name="street" className="form-control" placeholder='Street/Apartment'/>
                   </div>
                   </div>
 
                   <div className="row">
                   <div className="mb-3 col-6">
-                      <input type="text" name="" className="form-control" placeholder='Locality'/>
+                      <input onInput={e => {
+                        if(!user.address) user.address = {};
+                        user.address.locality = e.target.value;
+                      }} type="text" name="locality" className="form-control" placeholder='Locality'/>
                   </div>
 
                   <div className="mb-3 col-6">
-                      <input type="text" name="" className="form-control" placeholder='City'/>
+                      <input onInput={e => {
+                        if(!user.address) user.address = {};
+                        user.address.city = e.target.value;
+                      }} type="text" name="city" className="form-control" placeholder='City'/>
                   </div>
                   </div>
 
-                  <div className="mb-3 col-4">
-                      <input type="text" name="" className="form-control" placeholder='Pin code'/>
+                  <div className="mb-3 col-3">
+                      <input onInput={e => {
+                        if(!user.address) user.address = {};
+                        user.address.pinCode = e.target.value;
+                      }} type="text" name="pin" className="form-control" placeholder='Pin code'/>
                   </div>
 
                   <button type="submit" className="float-end btn btn-success">Register</button>
